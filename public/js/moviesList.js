c573af60dccd9e08948be3c9481bf873e805a271
jesica <jesicaguerrerof@gmail.com>
2022-01-11 01:18:45 -0300

    modo noche y listado de peliculas done

--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -1,1 +1,10 @@
 console.log('moviesList.js connected success')
+const body=document.querySelector('body')
+const h1=document.querySelector('.moviesListTitulo')
+
+const response=confirm('queres cambiar el modo oscuro?')
+if(response){
+    body.style.backgroundColor='#7f7f7f'
+    body.classList.add('fondoMoviesList')
+}
+h1.innerHTML='listado de peliculas'
