84733f108eaa4d46c4d6fbfc33413b2ed7ff456f
jesica <jesicaguerrerof@gmail.com>
2022-01-11 02:11:52 -0300

    captura de elementos ok

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -1,1 +1,10 @@
-console.log('moviesAdd.js connected success')
+console.log('moviesAdd.js connected success');
+const h1=document.querySelector('.moviesAddTitulo');
+const section=document.getElementById('formulario');
+const article=document.querySelector('article');
+
+h1.innerHTML=("AGREGAR PELICULAS")
+h1.classList.add('titulo')
+article.classList.add('fondoTransparente')
+section.classList.add('fondoCRUD')
+
